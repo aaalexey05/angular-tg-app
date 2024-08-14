58d00f801f931c5398c7489fdbeeb6c7b15502b5
aaalexey05 <alexeshilyaev@yandex.ru>
2024-08-14 12:06:51 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TelegramService } from './services/telegram.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet],
+  template: `<router-outlet />`,
 })
 export class AppComponent {
-  title = 'angular-tg-app';
+  telegram = inject(TelegramService);
+  constructor() {
+    this.telegram.ready();
+  }
 }
